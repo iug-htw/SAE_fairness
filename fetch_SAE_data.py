7fd3492da565ec6ba67a1964c733ac47a7caf325
iug-htw <54660219+iug-htw@users.noreply.github.com>
2024-12-09 13:13:11 +0000

    erster Versuch

--- fetch_SAE_data.py
+++ fetch_SAE_data.py
@@ -2,18 +2,28 @@ import requests
 import os
 
 url = "https://www.neuronpedia.org/api/explanation/search"
-
-payload = {
-    "modelId": "gemma-2b",
-    "layers": ["0-res-jb", "6-res-jb"],
-    "query": "muslim woman"
-}
 api_key = os.getenv("NEURONPEDIA_KEY")
 headers = {
-    "Content-Type": "application/json",
-    "X-Api-Key": api_key
+   "Content-Type": "application/json",
+   "X-Api-Key": api_key
 }
 
-response = requests.post(url, json=payload, headers=headers)
+def search_explanations_by_model():
+    payload = {
+        #Modelle: 
+        "modelId": "gemma-2b", 
+        #Schichten im Modell?
+        "layers": ["0-res-jb", "6-res-jb"],
+        "query": "muslim"
+    }
+    response = requests.post(url, json=payload, headers=headers)
+    print(response.json())
+
+
+#search_explanations_by_model()
+
+#Feature runterladen: Featurenummer muss in URL stehen
+url = "https://www.neuronpedia.org/api/feature/gpt2-small/0-res-jb/14057"
+response = requests.get(url, headers=headers)
 
 print(response.json())
