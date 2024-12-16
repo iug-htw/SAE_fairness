e4e1ed9000782180067e0241cbbe9d6e5c84752b
iug-htw <54660219+iug-htw@users.noreply.github.com>
2024-12-16 14:12:41 +0000

    add iterate model releases in fetch SAE data

--- step1_fetch_SAE_data.py
+++ step1_fetch_SAE_data.py
@@ -1,12 +1,29 @@
 import requests
 import os
 import json
-
-
+#this file should be extended so it can collect data for multiple models instead of only gpt2-small
+#data collected should be saved to a folder named after model name
+#Geht anscheinend nicht, da search by model nur für gpt2-small funktioniert
+releaseNames = ["llama3-8b-it-res-jh"]
+    #"gemma-scope","gpt2sm-kk",  
+    #"llamma-scope" hat nicht funktioniert
 api_key = os.getenv("NEURONPEDIA_KEY")
-modelId = "gpt2-small" 
+
 #prior queries not included anymore: see queries.json
-queries = ["fundamentalist"] # Query to search for
+queries = [
+    "christian",
+    "islam",
+    "jew",
+    "judaism",
+    "terrorist",
+    "cocaine",
+    "muslim",
+    "juif",
+    "christianity",
+    "homophob",
+    "fundamentalist",
+    "Jude"
+    ] # Query to search for
 
 headers = {
    "Content-Type": "application/json",
@@ -23,46 +40,48 @@ def save_queries(queries, filename='queries.json'):
     with open(filename, 'w') as file:
         json.dump(queries, file, indent=4)
 
-def search_latent_features_by_model(query):
+def search_latent_features_by_model(query,modelID): 
     #search by model aus API https://www.neuronpedia.org/api-doc#tag/explanations/POST/api/explanation/search-model
-    url = "https://www.neuronpedia.org/api/explanation/search-model"
+    url = "https://www.neuronpedia.org/api/explanation/search-release"
 
     payload = {
-        "modelId": modelId,
+        "releaseName": modelID,
         "query": query,
     }
     response = requests.post(url, json=payload, headers=headers)
+    print(response)
     response_data = response.json()
     
     # Create 'json' folder if it doesn't exist
-    if not os.path.exists('json/'+query):
-        os.makedirs('json/'+query)  
+    if not os.path.exists('json/'+modelID+'/'+query):
+        os.makedirs('json/'+modelID+'/'+query)  
     # Save the JSON response to a file in the 'json' folder
-    filename = 'json/'+query+'/explanation_for_query_'+query+'.json'
+    filename = 'json/'+modelID+'/'+query+'/explanation_for_query_'+query+'.json'
     with open(filename, 'w') as json_file:
         json.dump(response_data, json_file, indent=4)
     # Extract the featureId from the response
     features = []
     for result in response_data.get('results', []):
+        model= result.get('modelId')
         layer = result.get('layer')
         index = result.get('index')
-        features.append((layer, index))
+        features.append((model, layer, index))
     print(features)
     return features
 
 
-def search_explanations_by_feature(feature,query, modelID=modelId):
+def search_explanations_by_feature(feature,query):
     #get Feature aus API https://www.neuronpedia.org/api-doc#tag/features/GET/api/feature/{modelId}/{layer}/{index}
     #Featurenummer muss in URL stehen
-    layer, index = feature
+    model, layer, index = feature
     
-    url = "https://www.neuronpedia.org/api/feature/"+modelID+"/"+str(layer)+"/"+ str(index)
+    url = "https://www.neuronpedia.org/api/feature/"+model+'/'+str(layer)+"/"+ str(index)
     print(url)
     response = requests.get(url, headers=headers)
     
     # Save the JSON response to a file in the 'json' folder
     json_data = response.json()
-    filename = 'json/'+query+'/data_for_feature_' + str(index) + '.json'
+    filename = 'json/'+modelID+'/'+query+'/data_for_feature_' + str(index) + '.json'
     with open(filename, 'w') as json_file:
         json.dump(json_data, json_file, indent=4)
     print(f"Data saved to {filename}")
@@ -71,15 +90,17 @@ def search_explanations_by_feature(feature,query, modelID=modelId):
 
 
 # Main execution
-for query in queries:
-    features = search_latent_features_by_model(query)  # Search explanations by model
-    print(features)
-    output_filename = 'json/'+query+'/all_output_data_'+str(query)+'.json'
-    if features:
-        for feature in features:
-            search_explanations_by_feature(feature,query,modelId)
-    else:
-        print("Feature IDs not found in the response.")
+for releaseName in releaseNames:
+    modelID = releaseName
+    for query in queries:
+        features = search_latent_features_by_model(query,modelID)  # Search explanations by model
+        print(features)
+        output_filename = 'json/'+modelID+'/'+query+'/all_output_data_'+str(query)+'.json'
+        if features:
+            for feature in features:
+                search_explanations_by_feature(feature,query)
+        else:
+            print("Feature IDs not found in the response.")
 
 # Save the updated list of all queries
 existing_queries = load_existing_queries()
