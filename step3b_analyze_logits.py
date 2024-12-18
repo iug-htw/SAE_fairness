cff32ea33ab89f23dd254dc4ec5c426d3ba9a9ad
iug-htw <54660219+iug-htw@users.noreply.github.com>
2024-12-18 12:58:34 +0000

    step3b walks through model folder

--- step3b_analyze_logits.py
+++ step3b_analyze_logits.py
@@ -3,9 +3,10 @@ import os
 from collections import defaultdict
 
 #This checks for duplicate positive and negative strings in the logit data    
-# currently only look into one file
-#next step is to look into all files and compare across files
+
+
 #should also output table with all the duplicates, possibly in json format
+# should iterate across models
 
 
 def load_json_file(filename):
@@ -30,32 +31,30 @@ def find_redundancies(data):
     return redundant_positive, redundant_negative
 
 def main():
-    
     directory = 'json/gemma-scope'
-    all_files = []
 
     for root, _, files in os.walk(directory):
         for file in files:
             if file.startswith('logits_and_description') and file.endswith('.json'):
                 filepath = os.path.join(root, file)
+                print(f"Opening file from folder: {root}")
                 filename = load_json_file(filepath)
-                all_files.extend(filename)
+                redundant_positive, redundant_negative = find_redundancies(filename)
 
-    redundant_positive, redundant_negative = find_redundancies(all_files)
+                if redundant_positive:
+                    print("Redundant positive strings found:")
+                    for pos_str, indices in redundant_positive.items():
+                        print(f"'{pos_str}' found in indices: {', '.join(indices)}")
+                else:
+                    print("No redundant positive strings found.")
 
-    if redundant_positive:
-        print("Redundant positive strings found:")
-        for pos_str, indices in redundant_positive.items():
-            print(f"'{pos_str}' found in indices: {', '.join(indices)}")
-    else:
-        print("No redundant positive strings found.")
-
-    if redundant_negative:
-        print("Redundant negative strings found:")
-        for neg_str, indices in redundant_negative.items():
-            print(f"'{neg_str}' found in indices: {', '.join(indices)}")
-    else:
-        print("No redundant negative strings found.")
-
+                if redundant_negative:
+                    print("Redundant negative strings found:")
+                    for neg_str, indices in redundant_negative.items():
+                        print(f"'{neg_str}' found in indices: {', '.join(indices)}")
+                else:
+                    print("No redundant negative strings found.")
+                    # Write output to a JSON file
+    
 if __name__ == "__main__":
     main()
