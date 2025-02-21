814406ec0ec067eacd06dc55e251822f026ee40a
mariamkhmahran <mariamkhmahran@hotmail.com>
2025-02-21 14:48:39 +0100

    combine common features csv

--- step4b_combine_csv_inference.py
+++ step4b_combine_csv_inference.py
@@ -1,11 +1,17 @@
 import os
 import csv
 
-def combine_csv_files(model_source_sets, output_path):
+def combine_csv_files(model_sources, main_dir="json"):
+    model_sources_sets = []
+    for model in model_sources:
+        files = os.listdir(os.path.join(main_dir, model))
+        for source in files:
+            model_sources_sets.append((model, source))
+
     combined_data = {}
     
-    for model, source in model_source_sets:
-        input_path = f'json4/{model}/{source}/common_features.csv'
+    for model, source in model_sources_sets:
+        input_path = f'{main_dir}/{model}/{source}/common_features.csv'
         if os.path.exists(input_path):
             with open(input_path, 'r') as csvfile:
                 reader = csv.DictReader(csvfile)
@@ -17,34 +23,29 @@ def combine_csv_files(model_source_sets, output_path):
         else:
             print(f"File {input_path} does not exist.")
     
+    output_path = f'{main_dir}/combined_common_features.csv'
     with open(output_path, 'w', newline='') as csvfile:
-        fieldnames = ['Term1', 'Term2', 'Term3'] + [f'{model}-{source}' for model, source in model_source_sets]
+        fieldnames = ['Term1', 'Term2', 'Term3'] + [f'{model}-{source}' for model, source in model_sources_sets]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         
         writer.writeheader()
         for term_pair, features in combined_data.items():
             row = {'Term1': term_pair[0], 'Term2': term_pair[1], 'Term3': term_pair[2]}
-            for model, source in model_source_sets:
+            for model, source in model_sources_sets:
                 row[f'{model}-{source}'] = features.get(f'{model}-{source}', 0)
             writer.writerow(row)
 
+    print(f"✅ Combined CSV saved to {output_path}")
+
 if __name__ == "__main__":
-    model_source_sets = [
-        ("gpt2-small", "res-jb"),#fertig
-        ("gpt2-small", "att-kk"), #fertig
-        #("gpt2-small", "att_32k-oai"), #bis programmer
-        #("gpt2-small", "mlp_32k-oai"),bis programmer
-        ("gemma-2-2b", "gemmascope-att-16k"),# fertig
-        ("gemma-2-2b", "gemmascope-att-65k"),# fertig
-        ("gemma-2-2b", "gemmascope-mlp-16k"),# fertig
-        #("gemma-2-2b", "gemmascope-mlp-65k"), überspringen!
-        #("gemma-2-2b", "gemmascope-res-16k"), überspringen!
-        #("gemma-2-2b", "gemmascope-res-65k"), überspringen!
-        #("gemma-2-9b", "gemmascope-res-16k"), überspringen!
-        #("gemma-2-9b-it", "gemmascope-res-16k"), überspringen!
-        ("gemma-2-9b-it", "gemmascope-res-131k"),  # fertig
-        ("llama3.1-8b","llamascope-res-32k"),# fertig
+    model_sources = [
+        "gemma-2-2b",
+        "gemma-2-9b",
+        "gemma-2-9b-it",
+        "gpt2-small",
+        "llama3.1-8b",
     ]
+
     output_path = 'json4/combined_common_features.csv'
-    combine_csv_files(model_source_sets, output_path)
+    combine_csv_files(model_sources, main_dir="json4")
     print(f"Combined CSV saved to {output_path}")
